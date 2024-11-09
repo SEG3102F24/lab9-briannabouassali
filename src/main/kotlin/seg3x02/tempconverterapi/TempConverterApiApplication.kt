c2868743358f21d86f97cd8f6171495a90740cc2
briannabouassali <bboua059@uottawa.ca>
2024-11-08 21:11:39 -0500

    Committing code

--- src/main/kotlin/seg3x02/tempconverterapi/TempConverterApiApplication.kt
+++ src/main/kotlin/seg3x02/tempconverterapi/TempConverterApiApplication.kt
@@ -2,6 +2,7 @@ package seg3x02.tempconverterapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 class TempConverterApiApplication
---
+++ src/main/kotlin/seg3x02/tempconverterapi/controller/SecurityConfig.kt
@@ -0,0 +1,36 @@
+package seg3x02.tempconverterapi.controller
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig
+{
+    @Bean
+    fun userDetailsService(): UserDetailsService 
+    {
+        val user1 = User.withUsername("user1")
+            .password("{noop}pass1")
+            .roles("USER")
+            .build()
+        val user2 = User.withUsername("user2")
+            .password("{noop}pass2")
+            .roles("USER")
+            .build()
+        return InMemoryUserDetailsManager(user1, user2)
+    }
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain 
+    {
+        http
+            .authorizeRequests { auth -> auth.anyRequest().authenticated() }
+            .httpBasic()
+        return http.build()
+    }
+}
